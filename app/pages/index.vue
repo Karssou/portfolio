e9be04a4afdc4d05654a2586c5674550de09d167
Karssou <alexandrelarue44@gmail.com>
2026-07-12 18:59:38 +0200

    [UPDATE] Move the Project section to the top [ADD] Add the mention to my baccalauréat

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -133,6 +133,12 @@ usePortfolioMotion(pageRoot);
       </div>
     </section>
 
+    <section id="projects" class="scroll-mt-28 px-6 pt-24 sm:px-8 lg:px-12">
+      <div class="mx-auto max-w-7xl" data-section>
+        <LazyHomeProjectsSection />
+      </div>
+    </section>
+
     <section id="about" class="scroll-mt-28 px-6 pt-24 sm:px-8 lg:px-12">
       <div class="mx-auto max-w-7xl" data-section>
         <LazyHomeAboutSection />
@@ -158,12 +164,6 @@ usePortfolioMotion(pageRoot);
           <LazySkillsEducation />
           <LazySkillsLanguage />
         </div>
-      </div>
-    </section>
-
-    <section id="projects" class="scroll-mt-28 px-6 pt-24 sm:px-8 lg:px-12">
-      <div class="mx-auto max-w-7xl" data-section>
-        <LazyHomeProjectsSection />
       </div>
     </section>
 
