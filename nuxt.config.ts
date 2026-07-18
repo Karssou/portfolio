594185e9921b1adc4002c3d0137e1139a2736580
Karssou <alexandrelarue44@gmail.com>
2026-07-18 18:03:53 +0200

    [DEPENDENCIES] Add `Nuxt Content` & `Google Fonts` (Has it had mysteriously disappeared) [UPDATE] Update the `nuxt.config.ts` to make the icon-bundle of iconify local when deployed in prod

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     clientBundle: {
       scan: true,
     },
-    serverBundle: "remote",
+    serverBundle: "local",
   },
 
   app: {
@@ -63,6 +63,7 @@ export default defineNuxtConfig({
     "@vueuse/nuxt",
     "@nuxtjs/i18n",
     "@nuxtjs/sitemap",
+    "@nuxt/content",
   ],
 
   vite: {
